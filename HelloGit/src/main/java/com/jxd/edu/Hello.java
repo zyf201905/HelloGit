f7387df733d958ea58152afbf648501ab8027b48
zyf <780623969@qq.com>
2021-03-24 09:53:05 +0800

    第一次提交项目

---
+++ HelloGit/src/main/java/com/jxd/edu/Hello.java
@@ -0,0 +1,12 @@
+package com.jxd.edu;
+
+/**
+ * @author ZhouYufei
+ * @description Todo
+ * @date 2021-03-23 17:59
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+    }
+}
