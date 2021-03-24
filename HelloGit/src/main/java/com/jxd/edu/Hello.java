389e9f15ee783b71990ca2deda7f34739c41a250
zyf <780623969@qq.com>
2021-03-24 10:28:10 +0800

    创建分支dev

--- HelloGit/src/main/java/com/jxd/edu/Hello.java
+++ HelloGit/src/main/java/com/jxd/edu/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
         System.out.println("更新一");
+        System.out.println("创建分支dev");
     }
 }
